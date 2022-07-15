238101dcb2dc3563e188506370386279c02145b5
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-07-16 02:25:32 +0300

    adding counters for logs

--- app/app.py
+++ app/app.py
@@ -23,6 +23,8 @@ def main():
 
         cur.close()
         logger.info(f"Query returned {len(pages)} records")
+
+        total = 0
         i = 1
         for page in pages:
             logger.info(f"Parsing page {i}/{len(pages)}")
@@ -32,16 +34,22 @@ def main():
 
             logger.info(f"Found {len(list_rows)} items in page {i}/{len(pages)}")
 
+            k = 0
             for list_row in list_rows:
                 try:
                     list_item = ListItem(list_row)
 
                     table_model = list_item.convert_to_table_model()
                     table_model.create_entry()  # pyright: ignore [reportGeneralTypeIssues]
+                    k += 1
                 except AttributeError as e:
                     logger.warning(f"Could not parse item: {list_row} to {ListItem}")
 
+            total += k
+            logger.info(f"Page {i}/{len(pages)} done. Added {k} items")
             i += 1
+
+        logger.info(f"All pages parsed. Total items added: {total}")
 
 
 if __name__ == "__main__":
