0f0e6feefb7aa4a9babb697df0f8dde115860bd1
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-07-16 12:41:22 +0300

    remove indent

--- app/app.py
+++ app/app.py
@@ -65,32 +65,32 @@ def main():
         logger.info(f"Query returned {len(pages)} records")
         cur.close()
 
-        total = 0
-        i = 1
-        for page in pages:
-            logger.info(f"Parsing page {i}/{len(pages)}")
+    total = 0
+    i = 1
+    for page in pages:
+        logger.info(f"Parsing page {i}/{len(pages)}")
 
-            soup = BeautifulSoup(page, "html.parser")
-            list_rows = soup.findAll("tr", {"class", "list-row"})
+        soup = BeautifulSoup(page, "html.parser")
+        list_rows = soup.findAll("tr", {"class", "list-row"})
 
-            logger.info(f"Found {len(list_rows)} items in page {i}/{len(pages)}")
+        logger.info(f"Found {len(list_rows)} items in page {i}/{len(pages)}")
 
-            k = 0
-            for list_row in list_rows:
-                try:
-                    list_item = ListItem(list_row)
+        k = 0
+        for list_row in list_rows:
+            try:
+                list_item = ListItem(list_row)
 
-                    table_model = list_item.convert_to_table_model()
-                    table_model.create_entry()  # pyright: ignore [reportGeneralTypeIssues]
-                    k += 1
-                except AttributeError as e:
-                    logger.warning(f"Could not parse item: {list_row} to {ListItem}")
+                table_model = list_item.convert_to_table_model()
+                table_model.create_entry()  # pyright: ignore [reportGeneralTypeIssues]
+                k += 1
+            except AttributeError as e:
+                logger.warning(f"Could not parse item: {list_row} to {ListItem}")
 
-            total += k
-            logger.info(f"Page {i}/{len(pages)} done. Added {k} items")
-            i += 1
+        total += k
+        logger.info(f"Page {i}/{len(pages)} done. Added {k} items")
+        i += 1
 
-        logger.info(f"All pages parsed. Total items added: {total}")
+    logger.info(f"All pages parsed. Total items added: {total}")
 
     with create_connection(DATABASE_URL) as conn:
         cur = conn.cursor()
